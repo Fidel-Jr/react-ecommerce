7dbf896df570f40d532d8d6f443214d9457e99b8
Fidel <fidelsalongacolinares04@gmail.com>
2025-12-12 21:24:56 +0800

    Make create order button work

--- ecommerce-app/src/pages/tracking/OrderTracking.jsx
+++ ecommerce-app/src/pages/tracking/OrderTracking.jsx
@@ -59,7 +59,7 @@ function OrderTracking() {
                 Quantity: {orderProduct.quantity} 
             </div>
 
-            <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+            <img className="product-image" src={orderProduct.product.image} />
 
             <div className="progress-labels-container">
                 
